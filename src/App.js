7762ca084b279ded129ba953bbbf05e6e35bfbb6
Gabriel Wiggert <gabrielwiggert@gmail.com>
2022-03-14 12:45:39 -0300

    basic React structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Topo from './Topo.js';
 import Main from './Main';
+import FundoMobile from './FundoMobile';
 
 export default function App() {
     return (
         <div>
-					<Topo />
-					<Main />
-				</div>
+            <Topo />
+            <Main />
+            <FundoMobile />
+		</div>
     );
 }
