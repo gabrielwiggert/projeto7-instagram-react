657353127df9a3539dcad843da5b6d05e16b42be
Gabriel Wiggert <gabrielwiggert@gmail.com>
2022-03-14 17:26:59 -0300

    posts into array of objects + render w/ map method

--- src/Posts.js
+++ src/Posts.js
@@ -2,12 +2,15 @@ import TopoPost from './TopoPost';
 import FundoPost from './FundoPost';
 
 export default function Posts() {
+    const posts = [{imagemPost:"/img/gato-telefone.svg", imageUsuario:"/img/meowed.svg", userName:"meowed", curtidoPorImage:"/img/respondeai.svg",
+                    curtidoPorUser:"respondeai", numPessoas:"101.523"},
+                    {imagemPost:"/img/dog.svg", imageUsuario:"/img/barked.svg", userName:"Barked", curtidoPorImage:"/img/adorable_animals.svg",
+                    curtidoPorUser:"adorable_animals", numPessoas:"99.101"}];
+
     return (
         <div>
-            <Post imagemPost="/img/gato-telefone.svg" imageUsuario="/img/meowed.svg" userName="meowed" curtidoPorImage="/img/respondeai.svg"
-                curtidoPorUser="respondeai" numPessoas="101.523" />
-            <Post imagemPost="/img/dog.svg" imageUsuario="/img/barked.svg" userName="Barked" curtidoPorImage="/img/adorable_animals.svg"
-                curtidoPorUser="adorable_animals" numPessoas="99.101" />
+            {posts.map(post => <Post imagemPost={post.imagemPost} imageUsuario={post.imageUsuario} userName={post.userName}
+                        curtidoPorImage={post.curtidoPorImage} curtidoPorUser={post.curtidoPorUser} numPessoas={post.numPessoas}/>)}
         </div>
     );
 }
