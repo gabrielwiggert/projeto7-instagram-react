fa37fb7cb634b69ee29b35c2efb633e3c48dc0d4
oErco <erickrsilva@hotmail.com>
2022-03-18 11:10:55 -0300

    Preparando revisão de código

--- src/ListaIonIconsPost.js
+++
@@ -1,9 +0,0 @@
-export default function ListaIonIconsPost() {
-    const itens = ["heart-outline", "chatbubble-outline", "paper-plane-outline"];
-
-    return (
-        <div>
-            {itens.map(item => <ion-icon name={item}></ion-icon>)}
-        </div>
-    );
-}
